fef185cba37a976f8651b1b9c23477e536cf34ba
SAKSHI KUMARI <122092273+Sakshi-kumari19@users.noreply.github.com>
2025-06-09 18:39:27 +0530

    Create python-arithmetic-operators.py

---
+++ week_1_Assignment/python-arithmetic-operators.py
@@ -0,0 +1,11 @@
+''' python-arithmetic-operators: Involves performing basic mathematical operations like addition, subtraction, multiplication, and division. '''
+
+# Taking user input
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Performing Arithmetic operations
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
